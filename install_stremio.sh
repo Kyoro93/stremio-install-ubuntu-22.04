f9b52b8cd2760ad04728d07f0f334dd7af5e5744
Samuel Lopes Mastrofrancisco <contact@samuellm.dev>
2022-07-05 11:13:42 -0300

    Create install_stremio.sh

---
+++ install_stremio.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt install nodejs libmpv1 qml-module-qt-labs-platform qml-module-qtquick-controls qml-module-qtquick-dialogs qml-module-qtwebchannel qml-module-qtwebengine qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings librubberband2
+
+wget http://ftp.osuosl.org/pub/ubuntu/pool/multiverse/f/fdk-aac/libfdk-aac1_0.1.6-1_amd64.deb
+sudo dpkg -i libfdk-aac1_0.1.6-1_amd64.deb 
+
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.5_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1l-1ubuntu1.5_amd64.deb
+
+wget https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb
+sudo dpkg -i stremio_4.4.142-1_amd64.deb
