1292eeee9973de5285cc8ded5e4a5c95a298a681
Samuel Lopes Mastrofrancisco <contact@samuellm.dev>
2023-05-12 06:09:54 -0300

    Updated file name on install file install_stremio.sh

--- install_stremio.sh
+++ install_stremio.sh
@@ -5,7 +5,7 @@ wget http://ftp.osuosl.org/pub/ubuntu/pool/multiverse/f/fdk-aac/libfdk-aac1_0.1.
 sudo dpkg -i libfdk-aac1_0.1.6-1_amd64.deb 
 
 wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.22_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.20_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.22_amd64.deb
 
 wget https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb
 sudo dpkg -i stremio_4.4.142-1_amd64.deb
