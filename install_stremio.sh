c90427a060a7de5b6efaca49c734e80915a1b32d
Samuel Lopes Mastrofrancisco <contact@samuellm.dev>
2023-06-28 07:12:57 -0300

    Updated openssl link

--- install_stremio.sh
+++ install_stremio.sh
@@ -4,7 +4,7 @@ sudo apt install nodejs libmpv1 qml-module-qt-labs-platform qml-module-qtquick-c
 wget http://ftp.osuosl.org/pub/ubuntu/pool/multiverse/f/fdk-aac/libfdk-aac1_0.1.6-1_amd64.deb
 sudo dpkg -i libfdk-aac1_0.1.6-1_amd64.deb 
 
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.22_amd64.deb
+wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1-1ubuntu2.1~18.04.23_amd64.deb
 sudo dpkg -i libssl1.1_1.1.1-1ubuntu2.1~18.04.22_amd64.deb
 
 wget https://dl.strem.io/shell-linux/v4.4.142/stremio_4.4.142-1_amd64.deb
